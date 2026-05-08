24879f2cc0d6b5d2fc2da8bbad11cdda41001e1b
Mangali Sai <msai51553@gmail.com>
2026-05-08 15:05:37 +0530

    Add app configuration (CSS and asset paths)

---
+++ config.py
@@ -0,0 +1,12 @@
+CSS = """
+<style>
+    [data-testid="stHeader"] {
+            background: rgba(0,0,0,0);
+                }
+                    .stApp {
+                            color: #FFFFFF;
+                                }
+                                </style>
+                                """
+BACKGROUND = "assets/background.png"
+SIDE_BANNER = "assets/logo.png"
